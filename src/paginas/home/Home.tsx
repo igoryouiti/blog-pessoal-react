fb92f101e8225bfd2bd6d30d2f886306d5a5ba86
igoryouiti <igoryouiti@hotmail.com>
2022-08-25 12:15:16 -0300

    criacao e estlizacao componente Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <div>
+            <h1 className='titulo'>Home</h1>
+            <img className='img' src="https://i.imgur.com/u9RkTCj.jpg" 
+            alt="Rock Lee do Naruto dando joinha"/>
+        </div>
+    );
+}
+
+
+export default Home;
