b472bd0188aa5fb8a3837740925e54413befbe74
igoryouiti <igoryouiti@hotmail.com>
2022-08-26 19:20:20 -0300

    Criacao e implementacao de footer, home e navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Footer from './paginas/components/estaticos/footer/Footer';
+import Navbar from './paginas/components/estaticos/navbar/Navbar';
 import Home from './paginas/home/Home';
 
 function App() {
   return (
-    <div>
+    <>
+      <Navbar />
       <Home />
-    </div>
+      <Footer />
+    </>
   );
 }
 
