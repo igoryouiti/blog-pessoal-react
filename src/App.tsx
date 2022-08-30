9158bacb4b4903a75db451d33d9e68af952a5ce0
igoryouiti <igoryouiti@hotmail.com>
2022-08-30 16:26:29 -0300

    adicao de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+
+
 import Footer from './paginas/components/estaticos/footer/Footer';
 import Navbar from './paginas/components/estaticos/navbar/Navbar';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Home />
+      <div style={{ minHeight: '100vh'}}>
+        <Routes>
+          <Route  path="/" element= { <Login />} />
+          <Route  path="/login" element= { <Login />} />
+          <Route  path="/home" element= { <Home />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </Router>
   );
 }
 
