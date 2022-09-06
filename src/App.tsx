b66dcff660484692d92f9034f8974517cbbace4c
igoryouiti <igoryouiti@hotmail.com>
2022-09-06 11:16:02 -0300

    31 - Criando componentes postagem e temas Post,Put,Delete

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,11 @@ import './App.css';
 
 import Footer from './components/estaticos/footer/Footer';
 import Navbar from './components/estaticos/navbar/Navbar';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import ListaTema from './components/temas/listatema/ListaTema';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
@@ -24,6 +28,12 @@ function App() {
           <Route path="/cadastrousuario" element = {<CadastroUsuario />} />
           <Route path="/temas" element = {<ListaTema />} />
           <Route path="/posts" element = {<ListaPostagem />} />
+          <Route path="/formularioPostagem" element={<CadastroPost />} />
+          <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+          <Route path="/formularioTema" element={<CadastroTema />} />
+          <Route path="/formularioTema/:id" element={<CadastroTema />} />
+          <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+          <Route path="/deletarTema/:id" element={<DeletarTema />} />
         </Routes>
       </div>
       <Footer />
